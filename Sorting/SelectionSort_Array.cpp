cfa19202597955762d116830a9f2ffa0f4937aad
Jed <jed@jeddd.com>
2018-11-21 19:32:02 +0800

    20181121

---
+++ Sorting/SelectionSort_Array.cpp
@@ -0,0 +1,52 @@
+/* *************************
+* FileName: SelectionSort_Array.cpp
+* Author: Jed Zhang
+* Site: https://www.jedbit.com
+* Date: 2018.11.21
+* Description: 顺序表（数组）的选择排序算法。 
+**************************/
+#include <iostream>
+using namespace std;
+
+void swap(int& x, int& y) {
+	int temp = x;
+	x = y;
+	y = temp;
+}
+int find_max_pos(int data[], int bottom, int top) {
+	int maxPos = bottom;
+	for (int i = bottom + 1; i <= top; i++) {
+		if (data[i] > data[maxPos]) {
+			maxPos = i;
+		}
+	}
+	return maxPos;
+}
+void selection_sort(int data[], int n) {
+	for (int i = n - 1; i > 0; i--) {
+		int maxPos = find_max_pos(data, 0, i);
+		swap(data[i], data[maxPos]);
+	}
+}
+
+
+int main()
+{
+	int data[] = { 8, 4, 3, 5, 1, 7, 6, 2 };
+	int size = sizeof(data) / sizeof(data[0]);
+
+	for (int i = 0; i < size; i++) {
+		cout << data[i] << " ";
+	}
+	cout << endl;
+
+	selection_sort(data, size);
+
+	for (int i = 0; i < size; i++) {
+		cout << data[i] << " ";
+	}
+	cout << endl;
+
+	//system("pause");
+	return 0;
+}
