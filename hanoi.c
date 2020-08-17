0489d79ca90720cffeb4c7518a15f5b7d31237a2
Jed <jed@jeddd.com>
2020-08-17 15:08:35 +0800

    update hanoi.c

--- hanoi.c
+++ hanoi.c
@@ -1,43 +1,36 @@
-/* *************************
-* FileName: HanoiTower.c
-* Author: Jed Zhang
-* Site: https://www.jedbit.com
-* Date: 2017.10.31
-* Description: 汉诺塔问题的简单递归实现。
-**************************/
 #include <stdio.h>
-void move(int x, char orig, char targ, char trans);
-long stepNum(int y);
-int main(void)
-{
-	int n, steps;
-	scanf("%d", &n);
-	steps = stepNum(n);
-	if(steps == 0) printf("ERROR!");
-	//开始输出步骤
-	move(n, 'A', 'C', 'B');
-	printf("\nTotal %ld step(s)!\n\n", stepNum(n));
-	return 0;
+
+// 鍒╃敤閫氶」鍏紡璁＄畻鎬绘搷浣滄楠ゆ暟
+long stepNum(int y) {
+    if (y == 1)
+        return 1;
+    else if (y > 1)
+        return 2 * stepNum(y - 1) + 1;
+    else
+        return 0;
 }
 
-//操作步骤数量（递归）
-long stepNum(int y)
-{
-	if(y == 1)
-		return 1;
-	else if(y >1)
-		return 2*stepNum(y-1) + 1;
-	else return 0;
+// 閫掑綊姹傝В
+void move(int x, char orig, char targ, char trans) {
+    static long i = 0;
+    if (x == 1) {
+        printf("STEP %-2ld : %c -> %c\n", ++i, orig, targ);
+    } else {
+        move(x - 1, orig, trans, targ);
+        printf("STEP %-2ld : %c -> %c\n", ++i, orig, targ);
+        move(x - 1, trans, targ, orig);
+    }
 }
 
-void move(int x, char orig, char targ, char trans)
-{
-	static long i = 0;
-	if(1 == x) {
-		printf("STEP %-2d : %c -> %c\n", ++i, orig, targ);
-	} else {
-		move(x-1, orig, trans, targ);
-		printf("STEP %-2d : %c -> %c\n", ++i, orig, targ);
-		move(x-1, trans, targ, orig);
-	}
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    long steps = stepNum(n);
+    if (steps == 0) {
+        printf("ERROR!\n");
+    } else {
+        move(n, 'A', 'C', 'B');
+        printf("\nTotal %ld step(s).\n", steps);
+    }
+    return 0;
 }
