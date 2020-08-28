cf2fd65de958c7fefc7f261270e7824f8de48f5a
Jed <jed@jeddd.com>
2020-08-28 19:47:58 +0800

    format

--- Graph/digraph_traversal.cpp
+++ Graph/digraph_traversal.cpp
@@ -1,93 +1,92 @@
 #include <iostream>
-#include <vector>
 #include <list>
 #include <queue>
+#include <vector>
 using namespace std;
 
 class Digraph {
-public:
-	int vertexNum;    // Number of vertices
-	vector<vector<bool>> adj;	// Adjacency matrix presentation, unweighted directed graph
+  public:
+    int vertexNum;             // Number of vertices
+    vector<vector<bool>> adj;  // Adjacency matrix presentation, unweighted directed graph
 
-	Digraph(int n) :vertexNum(n) {
-		adj.resize(n);
-		for (auto it = adj.begin(); it != adj.end(); it++) {
-			it->resize(n);
-		}
-	}
+    Digraph(int n) : vertexNum(n) {
+        adj.resize(n);
+        for (auto it = adj.begin(); it != adj.end(); it++) {
+            it->resize(n);
+        }
+    }
 
-	void BFS();    // Print in BFS order
-	void DFS();    // Print in DFS order
+    void BFS();  // Print in BFS order
+    void DFS();  // Print in DFS order
 
-private:
-	void DFS_recursive(int& vertex, vector<bool> &visited);
+  private:
+    void DFS_recursive(int& vertex, vector<bool>& visited);
 };
 
 void Digraph::BFS() {
-	queue<int> que;
-	vector<bool> visited(vertexNum, false);
+    queue<int> que;
+    vector<bool> visited(vertexNum, false);
 
-	for (int i = 0; i < vertexNum; i++) {    // In case the graph is not connected
-		if (!visited[i]) {
-			que.push(i);
-			while (!que.empty()) {
-				int current = que.front();
-				que.pop();
-				if (!visited[current]) {
-					cout << current << " ";    // Visit the vertex
-					visited[current] = true;
-					for (int j = 0; j < vertexNum; j++) {
-						if (adj[current][j] == true && !visited[j]) {
-							que.push(j);
-						}
-					}
-				}
-			}
-		}
-	}
+    for (int i = 0; i < vertexNum; i++) {  // In case the graph is not connected
+        if (!visited[i]) {
+            que.push(i);
+            while (!que.empty()) {
+                int current = que.front();
+                que.pop();
+                if (!visited[current]) {
+                    cout << current << " ";  // Visit the vertex
+                    visited[current] = true;
+                    for (int j = 0; j < vertexNum; j++) {
+                        if (adj[current][j] == true && !visited[j]) {
+                            que.push(j);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 void Digraph::DFS() {
-	vector<bool> visited(vertexNum, false);
+    vector<bool> visited(vertexNum, false);
 
-	for (int i = 0; i < vertexNum; i++) {    // In case the graph is not connected
-		if (!visited[i]) {
-			DFS_recursive(i, visited);
-		}
-	}
+    for (int i = 0; i < vertexNum; i++) {  // In case the graph is not connected
+        if (!visited[i]) {
+            DFS_recursive(i, visited);
+        }
+    }
 }
 
-void Digraph::DFS_recursive(int& current, vector<bool> &visited) {
-	cout << current << " ";    // Visit the vertex
-	visited[current] = true;
-	for (int j = 0; j < vertexNum; j++) {
-		if (adj[current][j] && !visited[j]) {
-			DFS_recursive(j, visited);
-		}
-	}
+void Digraph::DFS_recursive(int& current, vector<bool>& visited) {
+    cout << current << " ";  // Visit the vertex
+    visited[current] = true;
+    for (int j = 0; j < vertexNum; j++) {
+        if (adj[current][j] && !visited[j]) {
+            DFS_recursive(j, visited);
+        }
+    }
 }
 
-int main()
-{
-	int n;
-	cin >> n;
-	Digraph g(n);
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			bool temp;
-			cin >> temp;
-			g.adj[i][j] = temp;
-		}
-	}
+int main() {
+    int n;
+    cin >> n;
+    Digraph g(n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            bool temp;
+            cin >> temp;
+            g.adj[i][j] = temp;
+        }
+    }
 
-	cout << endl;
-	cout << "DFS: ";
-	g.DFS();
-	cout << endl;
-	cout << "BFS: ";
-	g.BFS();
-	cout << endl;
+    cout << endl;
+    cout << "DFS: ";
+    g.DFS();
+    cout << endl;
+    cout << "BFS: ";
+    g.BFS();
+    cout << endl;
 
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
--- Graph/topological_sorting.cpp
+++ Graph/topological_sorting.cpp
@@ -1,111 +1,110 @@
 #include <iostream>
-#include <vector>
 #include <list>
 #include <queue>
+#include <vector>
 using namespace std;
 
 class Digraph {
-public:
-	int vertexNum;    // Number of vertices
-	vector<vector<bool>> adj;	// Adjacency matrix presentation, unweighted directed graph
+  public:
+    int vertexNum;             // Number of vertices
+    vector<vector<bool>> adj;  // Adjacency matrix presentation, unweighted directed graph
 
-	Digraph(int n) :vertexNum(n) {
-		adj.resize(n);
-		for (auto it = adj.begin(); it != adj.end(); it++) {
-			it->resize(n);
-		}
-	}
+    Digraph(int n) : vertexNum(n) {
+        adj.resize(n);
+        for (auto it = adj.begin(); it != adj.end(); it++) {
+            it->resize(n);
+        }
+    }
 
-	list<int> toposort_depth();
-	list<int> toposort_breadth();
+    list<int> toposort_depth();
+    list<int> toposort_breadth();
 
-private:
-	void toposort_depth_recursive(int current, vector<bool> &visited, list<int> &order);
+  private:
+    void toposort_depth_recursive(int current, vector<bool> &visited, list<int> &order);
 };
 
 list<int> Digraph::toposort_breadth() {
-	list<int> order;
-	vector<bool> visited(vertexNum, false);
-	vector<int> indegree(vertexNum, 0);
+    list<int> order;
+    vector<bool> visited(vertexNum, false);
+    vector<int> indegree(vertexNum, 0);
 
-	for (int i = 0; i < vertexNum; i++) {    // Make indegree table
-		for (int j = 0; j < vertexNum; j++) {
-			if (adj[i][j]) ++indegree[j];
-		}
-	}
+    for (int i = 0; i < vertexNum; i++) {  // Make indegree table
+        for (int j = 0; j < vertexNum; j++) {
+            if (adj[i][j]) ++indegree[j];
+        }
+    }
 
-	queue<int> que;    // Contains vertices whose indegree are 0
-	for (int i = 0; i < vertexNum; i++) {
-		if (indegree[i] == 0) {
-			que.push(i);
-		}
-	}
+    queue<int> que;  // Contains vertices whose indegree are 0
+    for (int i = 0; i < vertexNum; i++) {
+        if (indegree[i] == 0) {
+            que.push(i);
+        }
+    }
 
-	while (!que.empty()) {
-		int current = que.front();
-		order.push_back(current);
-		visited[current] = true;
-		que.pop();
-		for (int j = 0; j < vertexNum; j++) {
-			if (adj[current][j] && !visited[j]){
-				--indegree[j];
-				if (indegree[j] == 0) {
-					que.push(j);
-				}
-			}
-		}
-	}
+    while (!que.empty()) {
+        int current = que.front();
+        order.push_back(current);
+        visited[current] = true;
+        que.pop();
+        for (int j = 0; j < vertexNum; j++) {
+            if (adj[current][j] && !visited[j]) {
+                --indegree[j];
+                if (indegree[j] == 0) {
+                    que.push(j);
+                }
+            }
+        }
+    }
 
-	return order;
+    return order;
 }
 list<int> Digraph::toposort_depth() {
-	list<int> order;
-	vector<bool> visited(vertexNum);
-	for (int i = 0; i < vertexNum; i++) visited[i] = false;
+    list<int> order;
+    vector<bool> visited(vertexNum);
+    for (int i = 0; i < vertexNum; i++) visited[i] = false;
 
-	for (int i = 0; i < vertexNum; i++) {
-		if (!visited[i]) {
-			toposort_depth_recursive(i, visited, order);
-		}
-	}
+    for (int i = 0; i < vertexNum; i++) {
+        if (!visited[i]) {
+            toposort_depth_recursive(i, visited, order);
+        }
+    }
 
-	return order;
+    return order;
 }
 
 void Digraph::toposort_depth_recursive(int current, vector<bool> &visited, list<int> &order) {
-	visited[current] = true;
-	for (int j = 0; j < vertexNum; j++) {
-		if (adj[current][j] && !visited[j]) {
-			toposort_depth_recursive(j, visited, order);
-		}
-	}
-	order.push_front(current);
+    visited[current] = true;
+    for (int j = 0; j < vertexNum; j++) {
+        if (adj[current][j] && !visited[j]) {
+            toposort_depth_recursive(j, visited, order);
+        }
+    }
+    order.push_front(current);
 }
-int main()
-{
-	int n;
-	cin >> n;
-	Digraph g(n);
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			bool temp;
-			cin >> temp;
-			g.adj[i][j] = temp;
-		}
-	}
+int main() {
+    int n;
+    cin >> n;
+    Digraph g(n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            bool temp;
+            cin >> temp;
+            g.adj[i][j] = temp;
+        }
+    }
 
-	list<int> depthOrder = g.toposort_depth();
-	cout << "Depth Order: ";
-	for (auto it = depthOrder.begin(); it != depthOrder.end(); it++) {
-		cout << *it << " ";
-	}
-	cout << endl;
+    list<int> depthOrder = g.toposort_depth();
+    cout << "Depth Order: ";
+    for (auto it = depthOrder.begin(); it != depthOrder.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
 
-	list<int> breadthOrder = g.toposort_breadth();
-	cout << "Breadth Order: ";
-	for (auto it = breadthOrder.begin(); it != breadthOrder.end(); it++) {
-		cout << *it << " ";
-	}
-	cout << endl;
-	return 0;
+    list<int> breadthOrder = g.toposort_breadth();
+    cout << "Breadth Order: ";
+    for (auto it = breadthOrder.begin(); it != breadthOrder.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
+    return 0;
 }
