c36bd807541587e67fdb839f10fdd04116aeb249
Jed <jed@jeddd.com>
2020-09-20 09:17:27 +0800

    format sorting

--- sorting/heapsort.cpp
+++ sorting/heapsort.cpp
@@ -2,48 +2,47 @@
 using namespace std;
 
 void insert_heap(int data[], int current, int low, int high) {
-	int largePos = 2 * low + 1;
-	while (largePos <= high) {
-		if (largePos < high && data[largePos] < data[largePos + 1]) {
-			largePos += 1;
-		}
-		if (current > data[largePos]) { break; }
-		else {
-			data[low] = data[largePos];
-			low = largePos;
-			largePos = 2 * low + 1;
-		}
-	}
-	data[low] = current;
+    int largePos = 2 * low + 1;
+    while (largePos <= high) {
+        if (largePos < high && data[largePos] < data[largePos + 1]) {
+            largePos += 1;
+        }
+        if (current > data[largePos]) {
+            break;
+        } else {
+            data[low] = data[largePos];
+            low = largePos;
+            largePos = 2 * low + 1;
+        }
+    }
+    data[low] = current;
 }
 void heap_sort(int data[], int n) {
-	for (int i = n / 2 - 1; i >= 0; i--) {
-		insert_heap(data, data[i], i, n - 1);
-	}
-	for (int i = n - 1; i >= 0; i--) {
-		int current = data[i];
-		data[i] = data[0];
-		insert_heap(data, current, 0, i - 1);
-	}
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        insert_heap(data, data[i], i, n - 1);
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        int current = data[i];
+        data[i] = data[0];
+        insert_heap(data, current, 0, i - 1);
+    }
 }
 
+int main() {
+    int data[] = {8, 4, 3, 5, 1, 7, 6, 2};
+    int size = sizeof(data) / sizeof(data[0]);
 
-int main()
-{
-	int data[] = { 8, 4, 3, 5, 1, 7, 6, 2 };
-	int size = sizeof(data) / sizeof(data[0]);
+    for (int i = 0; i < size; i++) {
+        cout << data[i] << " ";
+    }
+    cout << endl;
 
-	for (int i = 0; i < size; i++) {
-		cout << data[i] << " ";
-	}
-	cout << endl;
+    heap_sort(data, size);
 
-	heap_sort(data, size);
+    for (int i = 0; i < size; i++) {
+        cout << data[i] << " ";
+    }
+    cout << endl;
 
-	for (int i = 0; i < size; i++) {
-		cout << data[i] << " ";
-	}
-	cout << endl;
-
-	return 0;
+    return 0;
 }
--- sorting/quicksort.cpp
+++ sorting/quicksort.cpp
@@ -2,43 +2,43 @@
 using namespace std;
 
 void swap(int& x, int& y) {
-	int temp = x;
-	x = y;
-	y = temp;
+    int temp = x;
+    x = y;
+    y = temp;
 }
 
-void quick_sort(int data[], int bottom, int top){
-	if(bottom < top){
-		int& pivot = data[bottom];
-		int last_small = bottom;
-		for(int i = bottom+1; i <= top; i++){
-			if(data[i] < pivot){
-				++last_small;
-				swap(data[last_small], data[i]);
-			} 
-		}
-		swap(pivot, data[last_small]);
-		
-		// last_small is pivot position.
-		quick_sort(data, bottom, last_small-1);
-		quick_sort(data, last_small+1, top);
-	}
-}
-int main()
-{
-	int data[] = { 43, 16, 27, 59, 50, 26, 31, 48, 62, 36, 13, 50, 15, 27, 15, 37, 37, 16, 37, 21, 22, 19, 41, 27, 8, 25, 65, 59, 60, 1, 51, 18, 52, 42, 57, 65, 3, 39, 51, 44, 49, 59, 34, 8, 64, 38, 60, 45, 48, 42 };
-	int size = sizeof(data) / sizeof(data[0]);
+void quick_sort(int data[], int bottom, int top) {
+    if (bottom < top) {
+        int& pivot = data[bottom];
+        int last_small = bottom;
+        for (int i = bottom + 1; i <= top; i++) {
+            if (data[i] < pivot) {
+                ++last_small;
+                swap(data[last_small], data[i]);
+            }
+        }
+        swap(pivot, data[last_small]);
 
-	for (int i = 0; i < size; i++) {
-		cout << data[i] << " ";
-	}
-	cout << endl;
-	
-	quick_sort(data, 0, size-1);
-	
-	for (int i = 0; i < size; i++) {
-		cout << data[i] << " ";
-	}
-	cout << endl;
-	return 0;
+        // last_small is pivot position.
+        quick_sort(data, bottom, last_small - 1);
+        quick_sort(data, last_small + 1, top);
+    }
+}
+int main() {
+    int data[] = {43, 16, 27, 59, 50, 26, 31, 48, 62, 36, 13, 50, 15, 27, 15, 37, 37, 16, 37, 21, 22, 19, 41, 27, 8,
+                  25, 65, 59, 60, 1,  51, 18, 52, 42, 57, 65, 3,  39, 51, 44, 49, 59, 34, 8,  64, 38, 60, 45, 48, 42};
+    int size = sizeof(data) / sizeof(data[0]);
+
+    for (int i = 0; i < size; i++) {
+        cout << data[i] << " ";
+    }
+    cout << endl;
+
+    quick_sort(data, 0, size - 1);
+
+    for (int i = 0; i < size; i++) {
+        cout << data[i] << " ";
+    }
+    cout << endl;
+    return 0;
 }
