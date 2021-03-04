bffbdd0ee221e53327f0ac1869021a1117d12a69
Jed <jed@jeddd.com>
2021-03-04 20:27:32 +0800

    update selection sort

--- sorting/selectionsort.cpp
+++ sorting/selectionsort.cpp
@@ -1,43 +1,22 @@
 #include <iostream>
+
+#include "common.h"
 using namespace std;
 
-void swap(int& x, int& y) {
-    int temp = x;
-    x = y;
-    y = temp;
-}
-int find_max_pos(int data[], int bottom, int top) {
-    int maxPos = bottom;
-    for (int i = bottom + 1; i <= top; i++) {
-        if (data[i] > data[maxPos]) {
-            maxPos = i;
+void selectionSort(vector<int> &nums) {
+    int len = nums.size();
+    for (int i = 0; i < len - 1; i++) {
+        int min_pos = i;
+        for (int j = i + 1; j < len; j++) {
+            if (nums[j] < nums[min_pos]) {
+                min_pos = j;
+            }
         }
-    }
-    return maxPos;
-}
-void selection_sort(int data[], int n) {
-    for (int i = n - 1; i > 0; i--) {
-        int maxPos = find_max_pos(data, 0, i);
-        swap(data[i], data[maxPos]);
+        swap(nums[i], nums[min_pos]);
     }
 }
 
 int main() {
-    int data[] = {8, 4, 3, 5, 1, 7, 6, 2};
-    int size = sizeof(data) / sizeof(data[0]);
-
-    for (int i = 0; i < size; i++) {
-        cout << data[i] << " ";
-    }
-    cout << endl;
-
-    selection_sort(data, size);
-
-    for (int i = 0; i < size; i++) {
-        cout << data[i] << " ";
-    }
-    cout << endl;
-
-    // system("pause");
+    testDefault(selectionSort);
     return 0;
 }
