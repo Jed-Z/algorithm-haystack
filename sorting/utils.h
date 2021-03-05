618842ef8b40136aa77cbead4087a1a796ba19b0
Jed <jed@jeddd.com>
2021-03-05 09:54:45 +0800

    update sorting utils

--- sorting/utils.h
+++ sorting/utils.h
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> td_random = {4, 9, 0, 5, 6, 2, 1, 7, 3, 8};
+std::vector<int> td_standard = {4, 9, 0, 5, 6, 2, 1, 7, 3, 8};
 std::vector<int> td_reversed = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 std::vector<int> td_duplicated = {3, 3, 0, 4, 3, 2, 5, 2, 1, 5};
 
@@ -31,11 +31,25 @@ void print(T arr[], size_t n) {
     }
 }
 
-void testDefault(void (*f)(std::vector<int>&)) {
-    f(td_random);
-    print(td_random);
-    f(td_reversed);
-    print(td_reversed);
-    f(td_duplicated);
-    print(td_duplicated);
+bool test(std::vector<int> nums, void (*algorithm)(std::vector<int>&), bool display = true) {
+    srand((unsigned)time(NULL));
+    std::vector<int> nums_stl = nums;
+    sort(nums_stl.begin(), nums_stl.end());
+    algorithm(nums);
+    bool isCorrect = (nums_stl == nums);
+    std::cout << (isCorrect ? "OK" : "ERR") << ' ';
+    if (display)
+        print(nums);
+    else
+        std::cout << std::endl;
+    return isCorrect;
+}
+
+void testDefault(void (*algorithm)(std::vector<int>&)) {
+    test(td_standard, algorithm);
+    test(td_reversed, algorithm);
+    test(td_duplicated, algorithm);
+    std::vector<int> td_random(10000);
+    for (int i = 0; i < 10000; i++) td_random[i] = rand();
+    test(td_random, algorithm, false);
 }
