13347baf1e6ddc3adea1ad424f39c3f037d17932
Jed <jed@jeddd.com>
2021-03-06 13:02:31 +0800

    update heap sort

--- sorting/heapsort.cpp
+++ sorting/heapsort.cpp
@@ -2,28 +2,30 @@
 using namespace std;
 
 void maxHeapify(vector<int>& nums, int low, int high) {
-    int current = nums[low];
-    int largePos = 2 * low + 1;
-    while (largePos <= high) {
-        if (largePos < high && nums[largePos] < nums[largePos + 1]) {
-            largePos += 1;
+    int large_pos = 2 * low + 1;
+    while (large_pos <= high) {
+        if (large_pos + 1 <= high && nums[large_pos] < nums[large_pos + 1]) {
+            large_pos += 1;  // Index of the larger of the two children nodes
         }
-        if (current > nums[largePos]) {
+        if (nums[low] > nums[large_pos]) {
             break;
         } else {
-            nums[low] = nums[largePos];
-            low = largePos;
-            largePos = 2 * low + 1;
+            swap(nums[low], nums[large_pos]);
+            low = large_pos;
+            large_pos = 2 * low + 1;
         }
     }
-    nums[low] = current;
 }
 
 void heapSort(vector<int>& nums) {
     int len = nums.size();
+
+    // Build the initial heap
     for (int i = len / 2 - 1; i >= 0; i--) {
         maxHeapify(nums, i, len - 1);
     }
+
+    // Extract elements one by one
     for (int i = len - 1; i >= 0; i--) {
         swap(nums[0], nums[i]);
         maxHeapify(nums, 0, i - 1);
